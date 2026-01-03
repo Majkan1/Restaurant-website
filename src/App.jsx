a7f697b8dffe7f0315b04bd3b36733e8e1489d76
Mikołaj Michalak <majkanart117@gmail.com>
2026-01-03 08:56:02 +0100

    Add few changes.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="app">
       <h1>Movies App</h1>
-    <p>This is my application about movies</p>
+      <p>This is my application about movies</p>
     </div>
   )
 }
